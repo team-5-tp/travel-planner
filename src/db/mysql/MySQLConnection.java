a7a765a2d2a7499f97113fb1c4ab860136735fa6
Ke <ke.zhou.usa@gmail.com>
2019-03-23 08:21:32 -0400

    backend user feature 1

---
+++ src/db/mysql/MySQLConnection.java
@@ -0,0 +1,111 @@
+package db.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import db.DBConnection;
+
+public class MySQLConnection implements DBConnection {
+	private Connection conn;
+
+	public MySQLConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+			conn = DriverManager.getConnection(MySQLDBUtil.URL);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void close() {
+		try {
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public boolean userInsert(String username, String password) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return false;
+		}
+		try {
+			String sql = "INSERT IGNORE INTO user VALUES (?, ?)";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, username);
+			ps.setString(2, password);
+			return ps.executeUpdate() == 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public void userDelete(int user_id) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return;
+		}
+
+		try {
+			String sql = "DELETE FROM user WHERE user_id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, user_id);
+			ps.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean userUpdate(String username_new, String password_new, String username, String password) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return false;
+		}
+		try {
+			String sql = "UPDATA user SET user = ?, password =? WHERE username = ? and password ?)";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, username_new);
+			ps.setString(2, password_new);
+			ps.setString(3, username);
+			ps.setString(4, password);
+			return ps.executeUpdate() == 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean userVerify(String username, String password) {
+		if (conn == null) {
+			return false;
+		}
+		try {
+			String sql="select * from users where username = ? and password= ? ";
+			PreparedStatement statement=conn.prepareStatement(sql);
+			statement.setString(1, username);
+			statement.setString(2, password);
+			ResultSet rs=statement.executeQuery();
+			while (rs.next()) {
+				return true;
+			}
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
---
+++ src/db/mysql/MySQLTableCreation.java
@@ -0,0 +1,45 @@
+package db.mysql;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+
+public class MySQLTableCreation {
+	public static void main(String[] args) {
+		try {
+			// Step 1 Connect to MySQL.
+			System.out.println("Connecting to " + MySQLDBUtil.URL);
+			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+			Connection conn = DriverManager.getConnection(MySQLDBUtil.URL);
+			
+			if (conn == null) {
+				return;
+			}
+			// Step 2 Drop tables in case they exist.
+			Statement statement = conn.createStatement();
+			String sql = "DROP TABLE IF EXISTS user";
+			statement.executeUpdate(sql);
+
+			// Step 3 Create new tables
+			sql = "CREATE TABLE user ("
+					+ "user_id INT AUTO_INCREMENT,"
+					+ "username NVARCHAR(255) NOT NULL,"
+					+ "password NVARCHAR(255) NOT NULL,"
+					+ "PRIMARY KEY (user_id)"
+					+ ")";
+			statement.executeUpdate(sql);
+
+
+			// Step 4: insert fake user 1111/3229c1097c00d497a0fd282d586be050
+			sql = "INSERT INTO user VALUES('1111', '3229c1097c00d497a0fd282d586be050')";
+			statement.executeUpdate(sql);
+
+			conn.close();
+			System.out.println("Import done successfully");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
